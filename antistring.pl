0a7f879c6fe0b8f39da53ddfccc4ae373119a924
Philip <noemail>
2014-03-22 20:15:28 +0000

    new example

--- antistring.pl
+++ antistring.pl
@@ -1,6 +1,9 @@
 use String::AntiString;
 use feature qw(say);
+use Carp::Always;
 
 say "hello world" - " world" + " earth";
 say "hi" - "i";
 say -"hi" + "hi, there";
+
+say (("hi" - "there" - "xyz")->representation);
